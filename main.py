31a5266cd5e740efc025ebb403110a9b75571edf
uribo <ur1b0.dev@gmail.com>
2022-03-05 22:23:03 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# python3.7で動作確認済み
+
+import serial
+import time
+import json
+import datetime
+import os
+import time
+
+DEBUG = True
+
+def logging(path, name, data):
+    timestamp = datetime.datetime.now()
+    filename = name + "_" + timestamp.strftime("%Y-%m-%d") + ".csv"
+    write_str = timestamp.strftime("%Y/%m/%d %H:%M:%S") + "," + data
+    path = path + "/" + name + "/"
+
+    os.makedirs(path, exist_ok=True)
+    f = open(path + filename, mode="a")
+    f.write(write_str + "\n")
+    f.close()
+
+# 設定値読み込み
+f = open("./config.json", "r")
+conf = json.loads(f.read())
+f.close()
+
+path = conf["basedir"] + "/" + conf["logdir_name"]
