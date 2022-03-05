bd83750d62f6fb66a6539951c4a9f27bf0118bac
uribo <ur1b0.dev@gmail.com>
2022-03-05 23:25:19 +0900

    とりあえず動く

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import datetime
 import os
 import time
+import subprocess
 
 DEBUG = True
 
@@ -14,11 +15,11 @@ def logging(path, name, data):
     timestamp = datetime.datetime.now()
     filename = name + "_" + timestamp.strftime("%Y-%m-%d") + ".csv"
     write_str = timestamp.strftime("%Y/%m/%d %H:%M:%S") + "," + data
-    path = path + "/" + name + "/"
+    path = path + "/"
 
     os.makedirs(path, exist_ok=True)
     f = open(path + filename, mode="a")
-    f.write(write_str + "\n")
+    f.write(write_str)
     f.close()
 
 # 設定値読み込み
@@ -26,4 +27,14 @@ f = open("./config.json", "r")
 conf = json.loads(f.read())
 f.close()
 
+data_old = ""
+cmd = 'vcgencmd get_throttled'
+
 path = conf["basedir"] + "/" + conf["logdir_name"]
+
+while conf["interval"]:
+    data = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0].decode('utf-8')
+    if data_old != data:
+        logging(path,conf["logdir_name"],str(data))
+    data_old = data
+    time.sleep(conf["interval"])
